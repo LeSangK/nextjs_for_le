20a6bdc45e964669fee75bcc5aa6cf94bdd8b922
LeSangK <49548510+LeSangK@users.noreply.github.com>
2023-03-11 10:37:52 +0900

    fix_project_into_ts

---
+++ pages/index.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+export default function Home() {
+  const [todos, setTodos] = useState<string[]>([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleAddTodo = () => {
+    if (inputValue !== '') {
+      setTodos([...todos, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  const handleDeleteTodo = (index: number) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <h1 className="title">Todo App</h1>
+      <div className="inputContainer">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Enter a task"
+          className="textInput"
+        />
+        <button className="addButton" onClick={handleAddTodo}>
+          Add
+        </button>
+      </div>
+      <ul className="todosList">
+        {todos.map((todo, index) => (
+          <li className="todoItem" key={index}>
+            {todo}
+            <button className="deleteButton" onClick={() => handleDeleteTodo(index)}>
+              X
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
